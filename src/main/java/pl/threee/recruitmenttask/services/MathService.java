880e9db668e4f7c3508542efebcaa56fbb3124ef
Jakub Łokieć <jakublokiec@wp.pl>
2019-10-17 19:21:51 +0200

    Add divide method to math service

--- src/main/java/pl/threee/recruitmenttask/services/MathService.java
+++ src/main/java/pl/threee/recruitmenttask/services/MathService.java
@@ -7,4 +7,12 @@ public class MathService {
     public double add(double val1, double val2) {
         return val1 + val2;
     }
+
+    public double divide(double val1, double val2) {
+        if (val2 == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero.");
+        } else {
+            return val1 / val2;
+        }
+    }
 }
--- src/test/java/pl/threee/recruitmenttask/services/MathServiceTest.java
+++ src/test/java/pl/threee/recruitmenttask/services/MathServiceTest.java
@@ -1,25 +1,51 @@
 package pl.threee.recruitmenttask.services;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MathServiceTest {
+    private MathService mathService;
+
+    @BeforeEach
+    public void init() {
+        mathService = new MathService();
+    }
+
     @Test
     public void testAdd_PositiveNumbers() {
-        MathService mathService = new MathService();
         assertEquals(3.5801, mathService.add(1.2345, 2.3456), 1e-10);
     }
 
     @Test
     public void testAdd_NegativeNumbers() {
-        MathService mathService = new MathService();
         assertEquals(-3.5801, mathService.add(-1.2345, -2.3456), 1e-10);
     }
 
     @Test
     public void testAdd_PositiveAndNegativeNumber() {
-        MathService mathService = new MathService();
         assertEquals(1.1111, mathService.add(-1.2345, 2.3456), 1e-10);
+    }
+
+    @Test
+    public void testDivide_DivisionByZeroThrowsException() {
+        assertThrows(IllegalArgumentException.class, () -> mathService.divide(1.2345, 0));
+    }
+
+    @Test
+    public void testDivide_PositiveNumbers() {
+        assertEquals(12.5, mathService.divide(31.25, 2.5), 1e-10);
+    }
+
+    @Test
+    public void testDivide_NegativeNumbers() {
+        assertEquals(0.526304570259208, mathService.divide(-1.2345, -2.3456), 1e-10);
+    }
+
+    @Test
+    public void testDivide_PositiveAndNegativeNumber() {
+        assertEquals(-0.526304570259208, mathService.divide(-1.2345, 2.3456), 1e-10);
     }
 }

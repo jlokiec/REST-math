4f4d6730c22695bf2548989a0006a1083e44f673
Jakub Łokieć <jakublokiec@wp.pl>
2019-10-17 19:37:41 +0200

    Add divide method to math REST controller

--- src/main/java/pl/threee/recruitmenttask/controllers/MathController.java
+++ src/main/java/pl/threee/recruitmenttask/controllers/MathController.java
@@ -2,10 +2,7 @@ package pl.threee.recruitmenttask.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.threee.recruitmenttask.dto.MathResultDto;
 import pl.threee.recruitmenttask.services.MathService;
 
@@ -13,6 +10,7 @@ import pl.threee.recruitmenttask.services.MathService;
 @RequestMapping("/math")
 public class MathController {
     private static final String ADD_PATH = "/add";
+    private static final String DIVIDE_PATH = "/div";
     private static final String VAL1_PARAM = "val1";
     private static final String VAL2_PARAM = "val2";
 
@@ -26,6 +24,12 @@ public class MathController {
     @PostMapping(value = ADD_PATH + "/{" + VAL1_PARAM + "}/{" + VAL2_PARAM + "}", produces = MediaType.APPLICATION_JSON_VALUE)
     public MathResultDto add(@PathVariable(VAL1_PARAM) double val1, @PathVariable(VAL2_PARAM) double val2) {
         double result = mathService.add(val1, val2);
+        return new MathResultDto(result);
+    }
+
+    @GetMapping(value = DIVIDE_PATH + "/{" + VAL1_PARAM + "}/{" + VAL2_PARAM + "}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public MathResultDto divide(@PathVariable(VAL1_PARAM) double val1, @PathVariable(VAL2_PARAM) double val2) {
+        double result = mathService.divide(val1, val2);
         return new MathResultDto(result);
     }
 }
--- src/test/java/pl/threee/recruitmenttask/controllers/MathControllerTest.java
+++ src/test/java/pl/threee/recruitmenttask/controllers/MathControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,5 +27,13 @@ public class MathControllerTest {
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().json("{\"value\": 3.5801}"));
+    }
+
+    @Test
+    public void testDivide_ValidArguments() throws Exception {
+        mockMvc.perform(get("/math/div/1.2345/2.3456"))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"value\": 0.5263045702592086}"));
     }
 }
